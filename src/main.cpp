485fc40679e31411508b660956270f205e8558b0
Kilian <wishii64@outlook.fr>
2025-10-18 17:13:44 +0200

    Create window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main()
-{    
+{
+    sf::RenderWindow window(sf::VideoMode(1280, 720), "Pong");
+
+    while(window.isOpen() && !sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+        sf::Event event;
+        while(window.pollEvent(event)) {
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.display();
+    }
     return 0;
 }
